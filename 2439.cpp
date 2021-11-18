0934147014d6fffa318fdba4da062d18b899cac1
hemaher0 <hemahero@naver.com>
2021-11-19 01:06:09 +0900

    2439.cpp
    So easy.

---
+++ 2439.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int N;
+	cin >> N;
+	for (int i = 0; i < N; i++)
+	{
+		int pivot = N - i - 1;
+		for (int j = 0; j<N; j++)
+		{
+			if (j < pivot)
+			{
+				cout << " ";
+			}
+			else
+			{
+				cout << "*";
+			}
+		}
+		cout << endl;
+	}
+}
