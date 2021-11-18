98f0d977a6f6c987ed2077511c51027a2aa46f5d
hemaher0 <hemahero@naver.com>
2021-11-19 00:32:18 +0900

    2438.cpp
    So easy.

---
+++ 2438.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int N = 0;
+	cin >> N;
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = i; j >= 0; j--)
+		{
+			cout << '*';
+		}
+		cout << endl;
+	}
+}
